b2800f00495f4491ce3c8214517e4007507bd824
Elias Torres <elias.torressn@outlook.com>
2022-12-17 12:47:43 -0300

    add exercise 2: Cuadrado

---
+++ EjerciciosClaseTresYCuatro/Ejercicio2/Cuadrado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//2. Crear una clase Cuadrado con método para calcular el área del mismo.
+
+
+namespace EjerciciosClaseTresYCuatro.Ejercicio2
+{
+    internal class Cuadrado
+    {
+        //public double _side { get; set; }
+        public double _side { get; set; }
+
+
+
+        public Cuadrado() { }
+
+        public double squareArea(double _side)
+        {
+            return Math.Pow(_side, 2); ;
+        }
+
+
+    }
+}
---
+++ EjerciciosClaseTresYCuatro/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using EjerciciosClaseTresYCuatro.Ejercicio2;
+using System;
+
+namespace EjerciciosClaseTresYCuatro
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            // Ejercicio 2 : área de un cuadrado
+
+            //Cuadrado square = new Cuadrado();
+            //Console.WriteLine("Tamaño lado:");
+            //try
+            //{
+            //    square._side = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("El área de un cuadrado de lado: " + square._side + " es de: " + square.squareArea(square._side));
+            //}
+            //catch (FormatException) 
+            //{
+
+            //    Console.WriteLine("No es un número.");
+            //}
+
+
+
+
+
+        }
+
+    }
+}
